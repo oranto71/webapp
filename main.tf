615001d5284fc16bbd73388b7d5f816e157030f4
ttnwt <103345581+ttnwt@users.noreply.github.com>
2022-07-09 13:27:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAQ6OEQFVN3CJ4BLM5"
-  secret_key = "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"
 }
 resource "aws_instance" "web_instances_plus1" {
   ami           = "ami-0533f2ba8a1995cf9"
