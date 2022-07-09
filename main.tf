2d30dac8f55fef04b1cb9ad735d7d9020a11da35
ttnwt <103345581+ttnwt@users.noreply.github.com>
2022-07-09 12:54:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAQ6OEQFVNRZMCRFBA"
-  secret_key = "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"
+  access_key = "AKIAQ6OEQFVN3CJ4BLM5"
+  secret_key = "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"
 }
 resource "aws_instance" "web_instances_plus1" {
   ami           = "ami-0533f2ba8a1995cf9"
