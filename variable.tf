dbb28d9568f77902fe8f3bc6943223c6052e7942
ttnwt <103345581+ttnwt@users.noreply.github.com>
2022-07-09 14:34:32 -0500

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "security_group" {
+    type = list(number)
+    default = [ 8000,9000,8080,80,22,23,443,360,300 ]
+}
+variable "instance_type" {
+    type = string
+    default = "t2.micro" 
+}
+variable "ami" {
+    type = string
+    default = "ami-0533f2ba8a1995cf9" 
